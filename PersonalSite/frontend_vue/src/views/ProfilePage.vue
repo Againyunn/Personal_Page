ce5407d79b51beb82c80f24c86ea5dcf2beacf48
Jaeyun Jung <76543591+Againyunn@users.noreply.github.com>
2022-08-28 19:49:20 +0900

    Feat: make detail contents & layout

--- PersonalSite/frontend_vue/src/views/ProfilePage.vue
+++ PersonalSite/frontend_vue/src/views/ProfilePage.vue
@@ -1,5 +1,25 @@
 <template>
-    <div></div>
+    <div class="profile-wrap">
+        <img src="../../public/static/img/profile.jpg" alt="" class="profile-img">
+        <div class="profile-content-wrap">
+            <div class="profile-content-title">
+                <span class="profile-title-head">정재윤(Jaeyun Jung)</span>
+                <br/>
+                <span class="profile-title-content">한국외국어대학교 GBT학부, 컴퓨터.전자시스템공학부</span>
+                <br/>
+                <span class="profile-title-content">GPA: 4.09/4.5</span>
+            </div>
+            <div class="profile-content-part">
+                <span class="profile-part-head">Awards</span>
+                <br/>
+                <span class="profile-part-content">2022 SW공동해커톤 우수상(SW중심대학협회장상)수상</span>
+                <br/>
+                <span class="profile-part-content">2021 GBT학부 캡스톤 디자인 경진대회 최우수상(경상대학장상)수상</span>
+                <br/>
+                <span class="profile-part-content">2021 SBA 웹 개발 프로그램 최우수상 수상</span>
+            </div>
+        </div>
+    </div>
 </template>
 <script>
 export default{
@@ -37,3 +57,47 @@ export default{
     }
 }
 </script>
+<style scoped>  
+    .profile-wrap{
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        margin-top: 80px;
+    }
+    .profile-wrap > .profile-img{
+        width: 300px;
+    }
+    .profile-wrap > .profile-content-wrap{
+        margin-left: 50px;
+        display: flex;
+        flex-direction: column;
+        flex-wrap: nowrap;
+        justify-content: left;
+        text-align: left;
+    }
+
+    .profile-wrap > .profile-content-wrap > .profile-content-title{
+        margin-bottom: 30px;
+    }
+    .profile-wrap > .profile-content-wrap > .profile-content-title > .profile-title-head{
+        font-size: 30px;
+        font-weight: bold;
+        text-shadow: 2px 2px 2px rgba(0 0 0 / 15%);
+    }
+    .profile-wrap > .profile-content-wrap > .profile-content-title > profile-title-content{
+        font-size: 20px;
+        font-weight: normal;
+    }
+
+    .profile-wrap > .profile-content-wrap > .profile-content-part{
+        margin-bottom: 20px;
+    }
+    .profile-wrap > .profile-content-wrap > .profile-content-part > .profile-part-head{
+        font-size: 25px;
+        font-weight: bold;
+    }
+    .profile-wrap > .profile-content-wrap > .profile-content-part > .profile-part-content{
+        font-size: 20px;
+        font-weight: normal;
+    }
+</style>

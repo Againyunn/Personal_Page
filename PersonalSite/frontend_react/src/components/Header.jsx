7de833cfa3ace0c5f4e5e39b6d3c4d41f952dcf8
Jaeyun Jung <76543591+Againyunn@users.noreply.github.com>
2022-09-03 14:50:25 +0900

    Create: Header

---
+++ PersonalSite/frontend_react/src/components/Header.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+// css
+import "../css/Header.css";
+
+function Header(props) {
+  const [headerChange, setHeaderChange] = useState(false);
+  const [menuFont, setMenuFont] = useState("메뉴");
+
+  const navigate = useNavigate();
+
+  const moveToMain = () => {
+    navigate("/");
+  };
+
+  const clickedMenu = () => {
+    if (menuFont === "메뉴") {
+      setMenuFont("닫기");
+      setHeaderChange(true);
+
+      navigate("/MenuActived");
+    } else {
+      setMenuFont("메뉴");
+      setHeaderChange(false);
+    }
+  };
+
+  return (
+    <div className="header-wrap">
+      {/* 헤더 */}
+      {!headerChange ? (
+        <header className="header-basic">
+          <div className="main-logo" onClick={moveToMain}>
+            <span className="big-font">Againyunn</span>
+            <span className="small-font">FrontEnd Dev</span>
+          </div>
+        </header>
+      ) : (
+        <header className="header-changed" onClick={moveToMain}>
+          <div className="main-logo-changed">
+            <span className="big-font">Againyunn</span>
+            <span className="small-font">FrontEnd Dev</span>
+          </div>
+        </header>
+      )}
+
+      {/* 메뉴 토글 */}
+      <div id="menu-toggle">
+        <input
+          type="checkbox"
+          id="toggle"
+          checked={headerChange}
+          onChange={clickedMenu}
+          hidden
+        />
+        <span className="menu-font">{menuFont}</span>
+
+        <label htmlFor="toggle" className="toggleSwitch">
+          <span className="toggleButton"></span>
+        </label>
+      </div>
+    </div>
+  );
+}
+
+export default Header;

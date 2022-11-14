9f2f043853b2c519a8d4fc4430689aaa084d64a4
JaeyunJungGeeksloft <76543591+Againyunn@users.noreply.github.com>
2022-11-15 00:07:56 +0900

    Feat: change activation of sidebar and mousedown effect

--- PersonalSite/frontend_react/src/components/sidebar/SidebarBlog.jsx
+++ PersonalSite/frontend_react/src/components/sidebar/SidebarBlog.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 // data
 import { sidebar } from "components/sidebar/sidebarData.js";
@@ -8,8 +8,18 @@ import "static/style/css/ContentPage.css";
 import "static/style/css/Sidebar.css";
 
 function SidebarBlog(props) {
+  const sidebarRef = useRef([]);
+
+  useEffect(() => {
+    if (!props.isActive) {
+      sidebarRef.current[0].classList.add("in-active");
+    } else {
+      sidebarRef.current[0].classList.remove("in-active");
+    }
+  }, [props.isActive]);
+
   return (
-    <div className="sidebar-wrap">
+    <div className="sidebar-wrap" ref={(e) => (sidebarRef.current[0] = e)}>
       {sidebar.map((el, idx) => {
         return (
           <div className="sidebar-container">

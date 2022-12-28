779bd8f2e6897b995226ceb0ec432d8ec7a7cb00
Jaeyun Jung <76543591+Againyunn@users.noreply.github.com>
2022-12-29 00:47:16 +0900

    Feat: use offcanvas in sidebar conponent
    - sidebar 기능을 bootstrap의 offcanvas로 전환

--- PersonalSite/frontend_react/src/components/sidebar/SidebarBlog.jsx
+++ PersonalSite/frontend_react/src/components/sidebar/SidebarBlog.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 // data
 import { sidebar } from "components/sidebar/sidebarData.js";
@@ -9,29 +10,71 @@ import { v4 as uuidv4 } from "uuid";
 // css
 import "static/style/css/ContentPage.css";
 import "static/style/css/Sidebar.css";
+import { Offcanvas } from "react-bootstrap";
 
 function SidebarBlog(props) {
   const sidebarRef = useRef([]);
 
-  useEffect(() => {
-    if (!props.isActive) {
-      sidebarRef.current[0].classList.add("in-active");
-    } else {
-      sidebarRef.current[0].classList.remove("in-active");
-    }
-  }, [props.isActive]);
+  // query-string 접근
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  // useEffect(() => {
+  //   if (!props.isActive) {
+  //     sidebarRef.current[0].classList.add("in-active");
+  //   } else {
+  //     sidebarRef.current[0].classList.remove("in-active");
+  //   }
+  // }, [props.isActive]);
+
+  const navigate = useNavigate();
+
+  const handleClose = () => {
+    props.setIsActive(!props.isActive);
+  };
+
+  const moveToLink = (linkTarget, paramId) => {
+    let queryStringLink = linkTarget + "?filter-id=" + paramId;
+    setSearchParams({ "filter-id": paramId });
+    navigate(queryStringLink);
+  };
 
   return (
-    <div className="sidebar-wrap" ref={(e) => (sidebarRef.current[0] = e)}>
-      {sidebar.map((el, idx) => {
-        return (
-          <div className="sidebar-container" key={() => uuidv4()}>
-            <span className="blue bold">{el.name}</span>
-          </div>
-        );
-      })}
-    </div>
+    <Offcanvas
+      show={props.isActive}
+      onHide={handleClose}
+      responsive={"sm" | "md" | "lg" | "xl" | "xxl"}
+    >
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title>기록.공부</Offcanvas.Title>
+      </Offcanvas.Header>
+      <Offcanvas.Body>
+        {sidebar.map((el, idx) => {
+          return (
+            <div className="sidebar-container" key={() => uuidv4()}>
+              <span
+                className="blue bold"
+                onClick={() => moveToLink(el.contentLink, el.contentKey)}
+              >
+                {el.name}
+              </span>
+            </div>
+          );
+        })}
+      </Offcanvas.Body>
+    </Offcanvas>
   );
+
+  // return (
+  //   <div className="sidebar-wrap" ref={(e) => (sidebarRef.current[0] = e)}>
+  //     {sidebar.map((el, idx) => {
+  //       return (
+  //         <div className="sidebar-container" key={() => uuidv4()}>
+  //           <span className="blue bold">{el.name}</span>
+  //         </div>
+  //       );
+  //     })}
+  //   </div>
+  // );
 }
 
 export default SidebarBlog;
--- PersonalSite/frontend_react/src/components/sidebar/sidebarData.js
+++ PersonalSite/frontend_react/src/components/sidebar/sidebarData.js
@@ -1,10 +1,17 @@
 export const sidebar = [
   {
     name: "javascript 공부",
-    url: "",
+    contentLink: "/blog",
+    contentKey: "js",
   },
   {
     name: "CS 공부",
-    url: "",
+    contentLink: "/blog",
+    contentKey: "cs",
+  },
+  {
+    name: "react.js 공부",
+    contentLink: "/blog",
+    contentKey: "react",
   },
 ];

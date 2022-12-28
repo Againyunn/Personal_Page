054bf856d4a9f966254d068efaaccae47f88e304
Jaeyun Jung <76543591+Againyunn@users.noreply.github.com>
2022-12-29 02:33:09 +0900

    Feat&Fix: change finger animation & click bug
    - click to see more 을 두번 클릭해야 하는 버그 해결
    (a태그 내에 onClick 이벤트를 부여하면 두번 클릭해야 작동, a태그 기본의 click과 충돌)

---
+++ PersonalSite/frontend_react/src/components/animation/effect/FingerAnimation.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function FingerAnimation(props) {
+  return (
+    <lottie-player
+      src="https://lottie.host/b4252b10-98b4-4667-b48b-a9c19474a463/8OaAMZAEQs.json"
+      background="transparent"
+      speed="1"
+      style={{ width: "30px;", height: "30px;" }}
+      loop
+      autoplay
+    ></lottie-player>
+  );
+}
+
+export default FingerAnimation;
